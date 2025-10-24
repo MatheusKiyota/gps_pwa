bd8187dfc2e3937fe423c05fd8661d38ec1dda90
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2025-10-24 17:17:54 -0400

    capturar localização

--- pwa-gps/js/main.js
+++ pwa-gps/js/main.js
@@ -1,2 +1,49 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+      console.log('🟢 Service worker registrada! 😎', reg);
+    } catch (err) {
+      console.log('🔴 Service worker registro falhou: ', err);
+    }
+  });
+}
 
-*
+let posicaoInicial; // variável para capturar a posição
+const capturarLocalizacao = document.getElementById('localizacao');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const mapa = document.getElementById("gmap_canvas")
+
+const sucesso = (posicao) => { // callback de sucesso para captura da posição
+  posicaoInicial = posicao;
+  latitude.innerHTML = posicaoInicial.coords.latitude;
+  longitude.innerHTML = posicaoInicial.coords.longitude;
+  mapa.src = "https://maps.google.com/maps?q=" + posicaoInicial.coords.latitude + "," + posicaoInicial.coords.longitude + "&z=19&ie=UTF8&iwloc=&output=embed" //prof
+};
+
+const erro = (error) => { // callback de erro (falha na captura de localização)
+  let errorMessage;
+  switch (error.code) {
+    case 0:
+      errorMessage = "Erro desconhecido";
+      break;
+    case 1:
+      errorMessage = "Permissão negada!";
+      break;
+    case 2:
+      errorMessage = "Captura de posição indisponível!";
+      break;
+    case 3:
+      errorMessage = "Tempo de solicitação excedido!";
+      break;
+  }
+
+  console.log('Ocorreu um erro: ' + errorMessage);
+};
+
+capturarLocalizacao.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition(sucesso, erro);
+});
+
